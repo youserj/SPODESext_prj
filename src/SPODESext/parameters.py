e2e6fdd636f2b17c1033fc3ac597cbbb1f66c106
vasilsv <youserj@mail.ru>
2025-09-12 15:07:51 +0300

    ver 0.0.4 * use     "DLMS_SPODES_client >= 0.19.1, <0.20",     "DLMS_SPODES >= 0.87.1" parameters.py: fix with new API task NEW: ChangeDisconnectControl

--- src/SPODESext/parameters.py
+++ src/SPODESext/parameters.py
@@ -2,6 +2,6 @@ from DLMS_SPODES.cosem_interface_classes import parameters as prs
 from DLMS_SPODES.cosem_interface_classes.parameter import Parameter
 
 
-SERIAL_NUMBER = prs.Data(Parameter.parse("0.0.96.1.0.255"))
-CLOSE_ELECTRIC_SEAL = prs.Data(Parameter.parse("0.0.96.51.6.255"))
-DEVICE_TYPE = prs.Data(Parameter.parse("0.0.96.1.1.255"))
+SERIAL_NUMBER = prs.Data.parse("0.0.96.1.0.255")
+CLOSE_ELECTRIC_SEAL = prs.Data.parse("0.0.96.51.6.255")
+DEVICE_TYPE = prs.Data.parse("0.0.96.1.1.255")
--- src/SPODESext/task.py
+++ src/SPODESext/task.py
@@ -1,8 +1,13 @@
 from dataclasses import dataclass
 from DLMS_SPODES.pardata import ParValues
 from StructResult import result
+from DLMS_SPODES.types import cdt, cst
+from DLMS_SPODES.cosem_interface_classes import disconnect_control
+from DLMS_SPODES.cosem_interface_classes import parameters as dlms_par
+from DLMS_SPODES.types.implementations import integers
 from DLMS_SPODES_client.client import Client
 from DLMS_SPODES_client import task
+from DLMS_SPODES import exceptions as exc
 from . import parameters as spodes_par
 
 
@@ -14,7 +19,7 @@ class SetSerialNumber(task.OK):
     async def exchange(self, c: Client) -> result.Ok | result.Error:
         return await task.WriteParValue(
             ParValues(
-                par=spodes_par.SERIAL_NUMBER.VALUE,
+                par=spodes_par.SERIAL_NUMBER.value,
                 data=self.data,
             )
         ).exchange(c)
@@ -26,8 +31,64 @@ class CloseSeal(task.OK):
 
     async def exchange(self, c: Client) -> result.Ok | result.Error:
         if isinstance((res := await task.WriteParValue(
-            ParValues(spodes_par.CLOSE_ELECTRIC_SEAL.VALUE, "0"),
+            ParValues(spodes_par.CLOSE_ELECTRIC_SEAL.value, "0"),
             msg="Запись пломбы").exchange(c)
         ), result.Error):
             return res
+        return result.OK
+
+
+@dataclass
+class ChangeDisconnectControlState(task.OK):
+    state: int
+    b: int = 0
+    msg: str = "Смена состояния DisconnectControl"
+
+    def __post_init__(self) -> None:
+        if not (0 <= self.state <= 2):
+            raise ValueError(F"got wrong {self.state=}, expect 0..2")
+
+    async def exchange(self, c: Client) -> result.Ok | result.Error:
+        """return output and control state"""
+        par = dlms_par.DisconnectControl.from_b(self.b)
+        obj = c.objects.getDISCONNECT_CONTROL(0)
+        if isinstance(res := await task.Sequence(
+            task.Par2Data[disconnect_control.ControlState](par.control_state),
+            task.Par2Data[disconnect_control.ControlMode](par.control_mode),
+            err_ignore=False
+        ).exchange(c), result.Error):
+            return res
+        state, mode = res.value
+        transitions: str = mode.get_letters(int(obj.control_mode))
+        match int(state), self.state:
+            case 0, 1 if "a" in transitions:
+                if isinstance(res2 := await task.Execute2(par.remote_reconnect, integers.Only0()).exchange(c), result.Error):
+                    return res2
+            case 0, 2 if "d" in transitions:
+                if isinstance(res2 := await task.Execute2(par.remote_reconnect, integers.Only0()).exchange(c), result.Error):
+                    return res2
+            case 1, 0 if "b" in transitions:
+                if isinstance(res2 := await task.Execute2(par.remote_disconnect, integers.Only0()).exchange(c), result.Error):
+                    return res2
+            case 2, 0 if "c" in transitions:
+                if isinstance(res2 := await task.Execute2(par.remote_disconnect, integers.Only0()).exchange(c), result.Error):
+                    return res2
+            case 2, 1 if "c" in transitions and "a" in transitions:
+                if isinstance(res3 := await task.Sequence(
+                    task.Execute2(par.remote_disconnect, integers.Only0()),
+                    task.Execute2(par.remote_reconnect, integers.Only0()),
+                    err_ignore=False
+                ).exchange(c), result.Error):
+                    return res3
+            case 1, 2 if "b" in transitions and "d" in transitions:
+                if isinstance(res3 := await task.Sequence(
+                    task.Execute2(par.remote_disconnect, integers.Only0()),
+                    task.Execute2(par.remote_reconnect, integers.Only0()),
+                    err_ignore=False
+                ).exchange(c), result.Error):
+                    return res3
+            case a, b if a == b:
+                return result.Error.from_e(exc.DLMSException("уже имеется <{disconnect_control.ControlState(self.state)}>"))
+            case _:
+                return result.Error.from_e(exc.DLMSException(F"переключение на <{disconnect_control.ControlState(self.state)}> невозможно из текущего <{obj.control_state}>"))
         return result.OK
